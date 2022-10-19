de18f6dac20e62e25d588ab14a50054b732cbe2d
R-Tsukada <r.tsukada14@gmail.com>
2022-10-19 21:50:36 +0900

    メールアドレス、入社日、お知らせ投稿権限をparamsに追加する

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -15,8 +15,6 @@ class EmployeesController < ApplicationController
   def create
     @employee = Employee.new(employee_params)
 
-    add_params
-
     if @employee.save
       redirect_to employees_url, notice: "社員「#{@employee.last_name} #{@employee.first_name}」を登録しました。"
     else
@@ -27,8 +25,6 @@ class EmployeesController < ApplicationController
   def edit; end
 
   def update
-    add_params
-
     if @employee.update(employee_params)
       redirect_to employees_url, notice: "社員「#{@employee.last_name} #{@employee.first_name}」を更新しました。"
     else
@@ -49,7 +45,8 @@ class EmployeesController < ApplicationController
   private
 
   def employee_params
-    params.require(:employee).permit(:number, :last_name, :first_name, :account, :password, :department_id, :office_id, :employee_info_manage_auth)
+    params.require(:employee).permit(:number, :last_name, :first_name, :account, :password, :department_id, :office_id, :employee_info_manage_auth, :email,
+                                     :date_of_joining, :news_posting_auth)
   end
 
   def set_employee
@@ -59,12 +56,6 @@ class EmployeesController < ApplicationController
   def set_form_option
     @departments = Department.all
     @offices = Office.all
-  end
-
-  # 現在、メールアドレスと入社日は入力できないため、ここで追加しています。
-  def add_params
-    @employee.email = 'sample@example.com' unless @employee.email
-    @employee.date_of_joining = Time.zone.today unless @employee.date_of_joining
   end
 
   def sort_column
