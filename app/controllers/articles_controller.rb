685f1b13b2fb7255047ed95f1f115f49ef854c8d
R-Tsukada <r.tsukada14@gmail.com>
2022-10-21 23:45:11 +0900

    お知らせ詳細を表示できるようにする

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,8 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all.order(created_at: 'DESC')
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
 end
