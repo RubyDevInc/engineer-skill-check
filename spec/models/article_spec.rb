2f575f7afac01d6bf90ee3f21120c90e93bdbe4c
shosuke1989 <shosuke.k.1989@googlemail.com>
2022-12-13 21:38:53 +0900

    day5 rspec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper' # 設定ファイルrails_helper.rbを読み込むコードが全テストにあります
 
 RSpec.describe Article, type: :model do
-  it "記事を投稿した時、タイトルが一文字以上、50文字以下であること" do
+  it "記事を投稿した時、バリデーションを通ること（タイトルが一文字以上、50文字以下）" do
     article = Article.new(
-            title:  "侍",
-            content: "太郎"
+            title:  "最初の記事",
+            content: "初めて投稿しました"
     )
-    expect(article.title).to eq "侍"
+    expect(article).to be_valid
   end
+  it "記事を投稿した時、バリデーションを通らないこと（タイトルが一文字以上、50文字以下）" do
+    article = Article.new(
+            title:  "",
+            content: "初めて投稿しました"
+    )
+    expect(article).to be_invalid
+  end
+
 end
